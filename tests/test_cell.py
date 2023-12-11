cc931e9f964ac40b4b1535202c18540d3b730b1b
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-11 14:10:18 -0600

    Flush out the cell class tests, but stopped when I recieved an error for ship value and I'm pivoting to flush out the ship class tests now

---
+++ tests/test_cell.py
@@ -0,0 +1,35 @@
+import unittest
+from lib.ship import Ship
+from lib.cell import Cell
+
+class TestCell(unittest.TestCase):
+  def setUp(self):
+    self.cell = Cell("B4")
+    self.cruiser = Ship("Cruiser", 3)
+
+  def test_exists(self):
+    self.assertIsInstance(self.cell, Cell)
+
+  def has_readable_attributes(self):
+    self.assertEqual(self.cell.coordinate, "B4")
+    self.assertIsNone(self.cell.ship)
+
+  def test_empty(self):
+    self.cell.place_ship(self.cruiser)
+    self.assertEqual(self.cell.ship, self.cruiser)
+    self.assertFalse(self.cell.empty_cell())
+
+  def test_place_ship(self):
+    self.assertTrue(self.cell.empty_cell())
+    self.cell.place_ship(self.cruiser)
+    self.assertEqual(self.cell.ship, self.cruiser)
+    self.assertFalse(self.cell.empty_cell())
+
+  def test_fired_upon(self):
+    self.cell.place_ship(self.cruiser)
+    self.assertFalse(self.cell.fired_upon)
+
+  def test_fire_upon(self):
+    self.cell.place_ship(self.cruiser)
+    self.cell.fire_upon
+    self.assertEqual(self.cell.ship.health, 2) # This line is breaking the test '3 != 2'. So it's not subtracting from the health of the ship when it is fired upon. Pivoting to make ship class tests now.
