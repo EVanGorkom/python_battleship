3ff550cb005b65b52a78fecdc6c51c8443db8d65
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-11 13:04:32 -0600

    Update the ship class for the python language

---
+++ lib/ship.py
@@ -0,0 +1,12 @@
+class Ship:
+  def __init__(self, name, length):
+    self.name = name
+    self.length = length
+    self.health = length
+
+  def hit(self):
+    self.health -= 1
+
+  def sunk(self):
+    if self.health == 0:
+      return True
