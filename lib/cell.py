d97b804806ad6ae488244b213d36dbebffdd9381
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-11 13:01:27 -0600

    Update the cell class for the python language

---
+++ lib/cell.py
@@ -0,0 +1,30 @@
+class Cell:
+  def __init__(self, coordinate):
+    self.coordinate = coordinate
+    self.ship = None
+    self.fired_upon = False
+  
+  def empty_cell(self):
+    return self.ship is None
+
+  def place_ship(self, boat):
+    self.ship = boat
+
+  def fired_upon(self):
+    return self.fired_upon
+  
+  def fire_upon(self):
+    if self.ship is not None and not self.fired_upon:
+      self.ship.hit()
+    self.fired_upon = True
+
+  def render(self, reveal_ship=False):
+    if self.fired_upon:
+      if self.empty():
+        return "M"
+      elif self.ship.sunk():
+        return "X"
+      elif reveal_ship and not self.empty():
+        return "S"
+      else:
+        return "*"
