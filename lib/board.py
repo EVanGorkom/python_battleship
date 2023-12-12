000e97c4c1f4ae3205ed453f9f930899c5bf50b0
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-11 22:12:17 -0600

    Build out more of the necessary board class methods

--- lib/board.py
+++ lib/board.py
@@ -1,8 +1,9 @@
-from cell import Cell
+from lib.cell import Cell
+from lib.ship import Ship
 import pdb
 
 class Board:
-  def __init__(self, cells):
+  def __init__(self):
     self.cells = self.create_cells
 
   def create_cells(self):
@@ -11,5 +12,14 @@ class Board:
       for num in range(1, 5):
         coordinate = f"{letter}{num}"
         cells[coordinate] = Cell(coordinate)
-    pdb.set_trace()
     return cells
+  
+  def valid_coordinate(self, coordinate):
+    return coordinate in self.cells
+  
+  def valid_placement(self, ship, coordinates):
+    if not (isinstance(ship, Ship) and isinstance(coordinates, list) and len(coordinates) == ship.length):
+      return False
+    
+    return all(self.valid_coordinate(coor) and self.cells[coor].empty() for coor in coordinates)
+  # still need to add in the consecutive? method from the ruby version
