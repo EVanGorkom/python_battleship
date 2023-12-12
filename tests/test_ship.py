acaeb5e4593f3158ee2da8bfeeaa584ba5bcfb2b
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-11 19:22:56 -0600

    Finish the ship class test

---
+++ tests/test_ship.py
@@ -0,0 +1,26 @@
+import unittest
+from lib.ship import Ship
+
+class TestShip(unittest.TestCase):
+  def setUp(self):
+    self.cruiser = Ship("Cruiser", 3)
+
+  def test_exists(self):
+    self.assertIsInstance(self.cruiser, Ship)
+
+  def has_readable_attributes(self):
+    self.assertEqual(self.cruiser.name, "Cruiser")
+    self.assertEqual(self.cruiser.length, 3)
+    self.assertEqual(self.cruiser.health, 3)
+
+  def test_hit(self):
+    self.assertEqual(self.cruiser.health, 3)
+    self.cruiser.hit()
+    self.assertEqual(self.cruiser.health, 2)
+
+  def test_sunk(self):
+    self.assertFalse(self.cruiser.sunk())
+    self.cruiser.hit()
+    self.cruiser.hit()
+    self.cruiser.hit()
+    self.assertTrue(self.cruiser.sunk())
