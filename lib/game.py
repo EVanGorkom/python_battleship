f6eeb83fe7ab7cd6b94e2cc24d85edd28e65f31e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-13 02:51:45 -0600

    Add more of the necessary game methods

--- lib/game.py
+++ lib/game.py
@@ -1,14 +1,16 @@
 from lib.board import Board
 from lib.cell import Cell
 from lib.ship import Ship
+import random
 import time
 import os
+import pdb
 
 class Game:
   def __init__(self):
     self.player_board = Board()
     self.computer_board = Board()
-    # self.ships = [Ship("Cruiser", 2), Ship("Battleship", 3)]
+    # self.ships = [Ship("Cruiser", 3), Ship("Submarine", 2), Ship("Battleship", 4)]
 
   def start(self):
     os.system('clear')
@@ -36,7 +38,9 @@ class Game:
         attempt_count += 1
 
       if user_input != 'p' and user_input != 'q' and attempt_count >= 7:
-        print("\n\nAlright, I'm ending this for both our sake's.")
+        print("\n\nYou leave me no choice.")
+        time.sleep(0.8)
+        print("I'm ending this for both of us.")
         time.sleep(1)
         print('3')
         time.sleep(1)
@@ -53,9 +57,9 @@ class Game:
       elif user_input != 'p' and user_input != 'q' and attempt_count >= 4:
         print("\n\nHaving a laugh are we?")
       elif user_input != 'p' and user_input != 'q' and attempt_count >= 3:
-        print("\n\nReally?")
+        print("\n\nReally? Try again.")
       else:  
-        print("\n\nWell that wasn't the letter 'p' or 'q' now was it? Are you sure you're qualified to operate a vessel of mass destruction soldier? \nTry agin, but watch what you type.")
+        print("\n\nWell that wasn't the letter 'p' or 'q' now was it? \nAre you sure you're qualified to operate a vessel of mass destruction soldier? \nTry agin, but watch what you type.")
 
   def play(self):
     os.system('clear')
@@ -99,13 +103,55 @@ class Game:
 
   def computer_place_ships(self):
     cruiser = Ship("Cruiser", 3)
-    submarine = Ship("Cruiser", 2)
+    submarine = Ship("Submarine", 2)
     ships = [cruiser, submarine]
-    
+    coordinates = []
+    for ship in ships:
+      while self.computer_board.valid_placement(ship, coordinates) == False:
+        coordinates = self.generate_ship_coors(ship)
+      self.computer_board.place(ship, coordinates)
+
+  def generate_ship_coors(self, ship):
+    starting_coor = self.generate_random_coordinate()
+    direction = random.randint(0, 1)
+    coordinates = [starting_coor]
+    if direction == 0:
+      for _ in range(ship.length - 1):
+        # pdb.set_trace() 
+        letter = starting_coor[0]
+        number = int(starting_coor[1]) + 1
+        next_coor = f'{letter}{number}'
+        coordinates.append(next_coor)
+    elif direction == 1:
+      for _ in range(ship.length - 1):
+        ord_letter = (ord(starting_coor[0]) + 1)
+        letter = chr(ord_letter)
+        number = starting_coor[1]
+        next_coor = f'{letter}{number}'
+        coordinates.append(next_coor)
+    return coordinates
+
+  def generate_random_coordinate(self):
+    rdm_coor = random.choice(list(self.player_board.cells.keys()))
+    return rdm_coor
 
   def player_place_ships(self):
-    return # add code here
-  
+    cruiser = Ship("Cruiser", 3)
+    submarine = Ship("Submarine", 2)
+    ships = [cruiser, submarine]
+    for ship in ships:
+      coordinates = self.get_users_coors(ship)
+      if self.player_board.valid_placement(ship, coordinates):
+        self.player_board.place(ship, coordinates)
+        print("==============Player Board==============\n")
+        self.player_board.render(True)
+        break
+      else:
+        print("An invalid placement of ships captain. Try again.")
+
+  def get_users_coors(self, ship):
+    input(f"Where shall we put our {ship} soldier?\nEnter {ship.length} coordinates you'd like separated with a space. \nEX: a2 b2 or A3 A4\n").upper()
+
   def player_turn(self):
     return # add code here
   
