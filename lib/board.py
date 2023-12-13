0046fadb92dca6baa9f3400fd9ea7066994a00f1
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-12 18:52:18 -0600

    Finish out the methods for the board class

--- lib/board.py
+++ lib/board.py
@@ -4,7 +4,7 @@ import pdb
 
 class Board:
   def __init__(self):
-    self.cells = self.create_cells
+    self.cells = self.create_cells()
 
   def create_cells(self):
     cells = {}
@@ -20,6 +20,33 @@ class Board:
   def valid_placement(self, ship, coordinates):
     if not (isinstance(ship, Ship) and isinstance(coordinates, list) and len(coordinates) == ship.length):
       return False
-    
-    return all(self.valid_coordinate(coor) and self.cells[coor].empty() for coor in coordinates)
-  # still need to add in the consecutive? method from the ruby version
+    return all(self.valid_coordinate(coor) and self.cells[coor].empty_cell() for coor in coordinates) and self.consecutive(coordinates)
+
+  def consecutive(self, coordinates):
+    chars_array = [coor[0] for coor in coordinates]
+    nums_array = [int(coor[1]) for coor in coordinates]
+    if len(set(chars_array)) == 1:
+      return all(nums_array[i] + 1 == nums_array[i + 1] for i in range(len(nums_array) - 1))
+    elif len(set(nums_array)) == 1:
+      return all(ord(chars_array[i] + 1 == chars_array[i + 1] for i in range(len(chars_array) - 1)))
+    else:
+      return False
+
+  def place(self, ship, coordinates):
+    if self.valid_placement(ship, coordinates):
+      for coordinate in coordinates:
+        self.cells[coordinate].place_ship(ship)
+
+  def render(self, reveal_ships=False):
+    board_render = "  " + ' '.join('ABCD') + "\n"
+    for row in range(1, 5):
+      board_render += str(row) + " "
+      for column in 'ABCD':
+        coordinate = f"{column}{row}"
+        cell = self.cells[coordinate]
+        board_render += cell.render(reveal_ships) + " "
+      board_render += "\n"
+    print(board_render)
+
+  def all_ships_sunk(self):
+    return all(cell.empty_cell() or cell.ship.sunk() for cell in self.cells.values())
