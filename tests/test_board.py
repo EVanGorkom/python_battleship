54403ad52602ad6cb5ec4b544082a5dbbddb48f4
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-12 18:52:42 -0600

    Finish creating the tests for the board class methods

--- tests/test_board.py
+++ tests/test_board.py
@@ -17,4 +17,34 @@ class TestBoard(unittest.TestCase):
     self.assertIsInstance(cells, dict)
 
   def test_creates_cells(self):
-    # self.assertTrue(self.board.valid_coordinate("A1")) # This still need fixing
+    expected_coordinates = set(f"{letter}{num}" for letter in "ABCD" for num in range(1, 5))
+    cells = self.board.create_cells()
+    self.assertIsInstance(cells, dict)
+    self.assertEqual(set(cells.keys()), expected_coordinates)
+
+  def test_valid_placement(self):
+    ship = Ship("TestShip", 2)
+    valid_coordinates = ["A1", "A2"]
+    self.assertTrue(self.board.valid_placement(ship, valid_coordinates))
+
+  def test_place_ship(self):
+    ship = Ship("TestShip", 2)
+    coordinates = ["A1", "A2"]
+    self.board.place(ship, coordinates)
+    for coordinate in coordinates:
+      cell = self.board.cells[coordinate]
+      self.assertEqual(cell.ship, ship)
+
+  def test_render(self):
+    ship = Ship("TestShip", 2)
+    coordinates = ["A1", "A2"]
+    self.board.place(ship, coordinates)
+
+  def test_all_ships_sunk(self):
+    ship = Ship("TestShip", 2)
+    coordinates = ["A1", "A2"]
+    self.board.place(ship, coordinates)
+    self.assertFalse(self.board.all_ships_sunk())
+    ship.hit()
+    ship.hit()
+    self.assertTrue(self.board.all_ships_sunk())
