60f3d03773655555016ab84695bcbc7a38b708f8
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2024-10-10 17:53:28 -0500

    Finish the major logic for the computer turn and implemented the 'smart computer' targeting system

--- lib/game.py
+++ lib/game.py
@@ -4,12 +4,15 @@ from lib.ship import Ship
 import random
 import time
 import os
+import pdb
 
 class Game:
   def __init__(self):
     self.player_board = Board()
     self.computer_board = Board()
     self.game_over = False
+    self.targeting_mode = False
+    self.targeting_mode_coordinates = []
     # self.ships = [Ship("Cruiser", 3), Ship("Submarine", 2), Ship("Battleship", 4)]
 
   def start(self):
@@ -62,8 +65,6 @@ class Game:
         print("\n\nWell that wasn't the letter 'p' or 'q' now was it? \nAre you sure you're qualified to operate a vessel of mass destruction soldier? \nTry agin, but watch what you type.")
 
   def check_game_over(self):
-    print("game over check")
-
     if self.player_board.all_ships_sunk():
       print("All you ships have been sunk!\n")
       art_folder = os.path.join(os.path.dirname(__file__), 'ascii_art')
@@ -72,6 +73,7 @@ class Game:
         content = file.read()
         print(content)
       self.game_over = True
+      time.sleep(4)
 
     elif self.computer_board.all_ships_sunk():
       print("You've sunk all the computer's ships!\n")
@@ -81,6 +83,7 @@ class Game:
         content = file.read()
         print(content)
       self.game_over = True
+      time.sleep(4)
 
   def play(self):
     os.system('clear')
@@ -95,18 +98,18 @@ class Game:
       self.player_turn()
       self.check_game_over()
       if self.game_over:
-        break
+        self.play_again()
 
       self.computer_turn()
       self.check_game_over()
       if self.game_over:
-        break
+        self.play_again()
 
   def play_again(self):
     os.system('clear')
     attempt_count = 1
     while True:
-      user_input = input("Do you want to play again?\nPress 'p' for Play\nPress 'q' for Quit\n")
+      user_input = input("\nDo you want to play again?\nPress 'p' for Play\nPress 'q' for Quit\n")
       if user_input.lower() == 'p':
         self.play()
         break
@@ -121,8 +124,10 @@ class Game:
         time.sleep(2)
         os.system('clear')
         print("You're welcome")
+        break
       elif attempt_count >= 2:
-        print("Oh, no thank you. \n'p' or 'q' will work fine.")
+        print("\nOh, no thank you. \n'p' or 'q' will work fine.")
+        attempt_count += 1
       else:
         attempt_count += 1
 
@@ -229,28 +234,60 @@ class Game:
     print("", end="", flush=True)
 
     for _ in range(3):
-      time.sleep(0.8)  # Pause between dots
+      time.sleep(0.8)
       print(".  ", end='', flush=True)
+    print()
 
-    print()  # Move to the next line after the dots
-
-    # Here you can add the logic for the computer's move
-    coordinate_to_fire = self.generate_random_coordinate()
+    if self.targeting_mode and len(self.targeting_mode_coordinates) != 0:
+      coordinate_to_fire = random.choice(self.
+      targeting_mode_coordinates)
+      self.targeting_mode_coordinates.remove(coordinate_to_fire)
+    else:
+      coordinate_to_fire = None
+      while coordinate_to_fire is None or self.player_board.cells[coordinate_to_fire].fired_upon_status():
+        coordinate_to_fire = self.generate_random_coordinate()
+          
     print(f"\nComputer fires at {coordinate_to_fire}!")
-
     target_cell = self.player_board.cells[coordinate_to_fire]
-
-    # Fire upon the chosen coordinate
     target_cell.fire_upon()
 
-    # Provide feedback for the computer's move
     if target_cell.empty_cell():
       print(f"\nMiss! {coordinate_to_fire} was an empty cell.")
     else:
       if target_cell.ship.sunk():
         print(f"\nDirect hit! The computer sunk your {target_cell.ship.name}!")
+        self.targeting_mode = False
       else:
         print(f"\nDirect hit at {coordinate_to_fire}! One of your ships was damaged.")
+        self.targeting_mode = True
+        self.find_adjacent_coordinates(coordinate_to_fire)
     
     time.sleep(0.8)
     time.sleep(0.8)
+
+  def find_adjacent_coordinates(self, coordinate: str):
+    letter = coordinate[0]
+    letter_ord = ord(letter)
+    number = int(coordinate[1])
+    
+    # I can add an additional check to make sure I'm not adding the same coordinate to the array more than once. something like `and not adjacent_coordinate in self.targeting_mode_coordinates`
+    if number > 1:
+      adjacent_coordinate = f"{letter}{number - 1}"
+      if self.player_board.valid_coordinate(adjacent_coordinate) and not self.player_board.cells[adjacent_coordinate].fired_upon:
+        self.targeting_mode_coordinates.append(adjacent_coordinate)
+
+    if number < 4:
+      adjacent_coordinate = f"{letter}{number + 1}"
+      if self.player_board.valid_coordinate(adjacent_coordinate) and not self.player_board.cells[adjacent_coordinate].fired_upon:
+        self.targeting_mode_coordinates.append(adjacent_coordinate) 
+
+    if letter_ord > 65:
+      adjacent_coordinate = f"{chr(letter_ord - 1)}{number}"
+      if self.player_board.valid_coordinate(adjacent_coordinate) and not self.player_board.cells[adjacent_coordinate].fired_upon:
+        self.targeting_mode_coordinates.append(adjacent_coordinate)
+
+    if letter_ord < 69:
+      adjacent_coordinate = f"{chr(letter_ord + 1)}{number}"
+      if self.player_board.valid_coordinate(adjacent_coordinate) and not self.player_board.cells[adjacent_coordinate].fired_upon:
+        self.targeting_mode_coordinates.append(adjacent_coordinate) 
+
